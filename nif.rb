991b08854ead441afcffd9060a26b714c31641c6
José Galisteo Ruiz <ceritium@gmail.com>
2010-11-22 20:53:15 +0100

    ¿base final? con reload

--- nif.rb
+++ nif.rb
@@ -1,6 +1,12 @@
 require 'rubygems'
 require 'sinatra'
 
+configure :development do |config|
+  require "sinatra/reloader"
+  config.also_reload "models/*.rb"
+end
+
+
 get '/' do
-  'Hello world!'
+  'Hello world! sdds'
 end
